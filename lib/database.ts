db61004cf10a0225d2ec2e78febe68bd2d544641
williampeterpaul <williampeterpaul@pm.me>
2023-06-06 16:38:04 +0100

    update db

--- lib/database.ts
+++ lib/database.ts
@@ -40,9 +40,9 @@ const connectionOptions = {
     password: process.env.TYPEORM_PASSWORD,
     synchronize: process.env.TYPEORM_SYNCHRONIZE == 'true',
     logging: process.env.TYPEORM_LOGGING == 'true',
-    // cache: {
-    //   duration: 15000
-    // },
+    cache: {
+      duration: 15000
+    },
     entities: [
       AuditSchema,
       OrganisationSchema,
