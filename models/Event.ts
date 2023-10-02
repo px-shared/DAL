d84511cda7fd20f71c5d927ff9abafbe80aeee29
williampeterpaul <williampeterpaul@pm.me>
2023-10-02 15:51:45 +0100

    Add event model

---
+++ models/Event.ts
@@ -0,0 +1,70 @@
+export default interface Event {
+  id: number;
+
+  action: string;
+  source: string;
+  referrer: string;
+  properties: any;
+  meta: any;
+  cfid: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+}
+
+export default class Event {}
+
+// export const EventSchema = new EntitySchema<Event>({
+//   name: 'Event',
+//   target: Event,
+//   columns: {
+//     id: {
+//       type: 'int',
+//       primary: true,
+//       generated: true
+//     },
+//     action: {
+//       type: 'varchar',
+//       length: 128
+//     },
+//     source: {
+//       type: 'varchar'
+//     },
+//     referrer: {
+//       type: 'varchar',
+//       nullable: true
+//     },
+//     properties: {
+//       type: 'simple-json',
+//       nullable: true
+//     },
+//     meta: {
+//       type: 'simple-json',
+//       nullable: true
+//     },
+//     cfid: {
+//       type: 'varchar'
+//     },
+//     createdAt: {
+//       type: 'timestamp',
+//       createDate: true
+//     },
+//     updatedAt: {
+//       type: 'timestamp',
+//       updateDate: true
+//     },
+//     deletedAt: {
+//       type: 'timestamp',
+//       deleteDate: true
+//     }
+//   },
+//   relations: {
+//     session: {
+//       type: 'many-to-one',
+//       target: 'Session',
+//       inverseSide: 'events',
+//       onDelete: 'CASCADE'
+//     }
+//   }
+// });
