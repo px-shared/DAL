f634fd72efada274166cacb73cd046d7c2160963
williampeterpaul <williampeterpaul@pm.me>
2023-10-02 18:02:56 +0100

    organisation: string;

--- models/Event.ts
+++ models/Event.ts
@@ -6,7 +6,7 @@ export default interface Event {
   referrer: string;
   properties: any;
   meta: any;
-  cfid: string;
+  organisation: string;
 
   createdAt: Date;
   updatedAt: Date;
@@ -14,57 +14,3 @@ export default interface Event {
 }
 
 export default class Event {}
-
-// export const EventSchema = new EntitySchema<Event>({
-//   name: 'Event',
-//   target: Event,
-//   columns: {
-//     id: {
-//       type: 'int',
-//       primary: true,
-//       generated: true
-//     },
-//     action: {
-//       type: 'varchar',
-//       length: 128
-//     },
-//     source: {
-//       type: 'varchar'
-//     },
-//     referrer: {
-//       type: 'varchar',
-//       nullable: true
-//     },
-//     properties: {
-//       type: 'simple-json',
-//       nullable: true
-//     },
-//     meta: {
-//       type: 'simple-json',
-//       nullable: true
-//     },
-//     cfid: {
-//       type: 'varchar'
-//     },
-//     createdAt: {
-//       type: 'timestamp',
-//       createDate: true
-//     },
-//     updatedAt: {
-//       type: 'timestamp',
-//       updateDate: true
-//     },
-//     deletedAt: {
-//       type: 'timestamp',
-//       deleteDate: true
-//     }
-//   },
-//   relations: {
-//     session: {
-//       type: 'many-to-one',
-//       target: 'Session',
-//       inverseSide: 'events',
-//       onDelete: 'CASCADE'
-//     }
-//   }
-// });
