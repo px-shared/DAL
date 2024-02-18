614a8d0fdf8491bb8b22109ee58e16cc5c39a118
williampeterpaul <williampeterpaul@pm.me>
2024-02-18 09:06:56 +0000

    export const createAudit = async (

--- lib/audit.ts
+++ lib/audit.ts
@@ -2,6 +2,7 @@ import { Connection } from 'typeorm';
 import Audit, { AuditSchema } from '../models/Audit';
 import Organisation from '../models/Organisation';
 import User from '../models/User';
+import Workspace from '../models/Workspace';
 
 export const createAudit = async (
   connection: Connection,
@@ -9,21 +10,21 @@ export const createAudit = async (
   entity: string,
   entityId: number | string,
   user: User,
-  organisation: Organisation,
+  workspace: Workspace,
   action: 'INSERT' | 'UPDATE' | 'REMOVE'
 ) => {
   try {
     if (!connection) return;
-    if (!organisation) return;
+    if (!workspace) return;
 
     return connection.manager.insert(
       AuditSchema,
-      new Audit(action, entity, entityId, payload, user, organisation)
+      new Audit(action, entity, entityId, payload, user, workspace)
     );
   } catch (error) {
     console.error(error);
     console.error(
-      `Failed to log audit for entity action:${action} type:${entity} id:${entityId} organisation:${organisation?.id}`
+      `Failed to log audit for entity action:${action} type:${entity} id:${entityId} workspace:${workspace?.id}`
     );
   }
 };
